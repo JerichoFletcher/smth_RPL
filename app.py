836359c845a8712915700baacece9beded669df6
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2023-04-06 16:54:59 +0700

    remove timestamp

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from datetime immport datetime
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     now = datetime.now()
-    return f"Hello from 13521107 on {now.strftime("%H:%M:%S")}!"
+    return f"Hello from 13521107 add branch!"
